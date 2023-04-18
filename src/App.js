34ce44ea9ed37b70ee030e6f835d0fc43eb0a7e7
LearningSignal <learningsignal@gmail.com>
2023-04-18 17:21:00 +0900

    mod

--- src/App.js
+++ src/App.js
@@ -3,22 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+		<div className="App">
+			<iframe
+				id="inlineFrameExample"
+				title="Inline Frame Example"
+				width="1366px"
+				height="1024px"
+				src="https://dive.hyundaicard.com/web/main.hdc"
+			></iframe>
+		</div>
   );
 }
 
