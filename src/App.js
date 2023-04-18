1f90bb91075e288c1effd2f4e6b18d209d962d51
LearningSignal <learningsignal@gmail.com>
2023-04-18 17:52:01 +0900

    mod

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import './App.css';
 function App() {
   return (
 		<div className="App">
-			<iframe
-				style={{ position: "absolute", width: "100%", height: "100%" }}
-				width="1280"
-				height="720"
-				src="https://dive.hyundaicard.com/web/main.hdc"
-				frameborder="0"
-				allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-				allowfullscreen=""
-			></iframe>
+			<div class="videowrapper">
+				<iframe
+					// width="560"
+					// height="315"
+					src="https://dive.hyundaicard.com/web/main.hdc"
+					frameborder="0"
+					allowfullscreen
+				></iframe>
+			</div>
 		</div>
   );
 }
